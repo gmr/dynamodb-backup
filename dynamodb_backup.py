5c0d367dc7ecf94e56d4d4cc0681209ef911ac26
Gavin M. Roy <gavinr@aweber.com>
2017-01-13 17:46:21 -0500

    Upload to S3, get schema via HTTP

--- dynamodb_backup.py
+++ dynamodb_backup.py
@@ -5,18 +5,23 @@ Backup DynamoDB to an Avro Container
 """
 import argparse
 import base64
+import coloredlogs
+import datetime
+import io
 import json
 import logging
+import sys
 import time
 
 import boto3
 import fastavro
+import requests
 try:
     import snappy
 except ImportError:
     snappy = None
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 LOGGER = logging.getLogger(__name__)
 
@@ -24,8 +29,12 @@ CODEC_CHOICES = {'none', 'deflate'}
 if snappy:
     CODEC_CHOICES.add('snappy')
 
-LOG_FORMAT = '%(asctime)-15s %(module)s: %(message)s'
-
+LOGGING_FORMAT = '%(asctime)s %(message)s'
+LOGGING_FIELD_STYLES = {'hostname': {'color': 'magenta'},
+                        'programname': {'color': 'cyan'},
+                        'name': {'color': 'blue'},
+                        'levelname': {'color': 'white', 'bold': True},
+                        'asctime': {'color': 'white'}}
 records, units = 0, 0
 
 
@@ -39,20 +48,46 @@ def backup(args):
     paginator = client.get_paginator('scan')
 
     start_time = time.time()
-    print('Preparing to backup {} from DynamoDB'.format(args.table))
+
+    if args.schema.startswith('http'):
+        response = requests.get(args.schema)
+        schema = response.json()
+    else:
+        with open(args.schema, 'r') as handle:
+            schema = json.load(handle)
+
+    if args.destination:
+        handle = open(args.destination, 'wb')
+        dest = args.destination
+    elif args.bucket:
+        handle = io.BytesIO()
+        dest = 's3://{}/{}'.format(args.bucket, args.prefix)
+    else:
+        LOGGER.error('ERROR: Must specify an error or an S3 bucket to use')
+        sys.exit(1)
+
+    LOGGER.info('Preparing to backup %s from DynamoDB to %s',
+                args.table, dest)
 
     try:
-        with open(args.destination, 'wb') as handle:
-            fastavro.writer(
-                handle, json.load(args.schema),
-                _backup(paginator, args.table),
-                args.codec,
-                validator=None if not args.skip_validation else True)
-    except KeyboardInterrupt:
-        print('CTRL-C caught, aborting backup')
+        fastavro.writer(
+            handle, schema,
+            _backup(paginator, args.table),
+            args.codec,
+            validator=None if not args.skip_validation else True)
 
-    print('Wrote {:,} records, consuming {:,} DynamoDB units '
-          'in {:.2f} seconds'.format(records, units, time.time() - start_time))
+    except KeyboardInterrupt:
+        LOGGER.error('CTRL-C caught, aborting backup')
+        sys.exit(1)
+
+    LOGGER.info(
+        'Wrote {:,} records, consuming {:,} DynamoDB units in '
+        '{:.2f} seconds'.format(records, units, time.time() - start_time))
+
+    if args.destination:
+        handle.close()
+    elif args.bucket:
+        _upload_to_s3(args, handle)
 
 
 def _backup(paginator, table):
@@ -78,10 +113,14 @@ def _backup(paginator, table):
 def main():
     """Setup and run the backup."""
     args = parse_cli_args()
+    level = logging.DEBUG if args.verbose else logging.INFO
+    coloredlogs.install(level=level, fmt=LOGGING_FORMAT,
+                        field_styles=LOGGING_FIELD_STYLES)
     if args.verbose:
-        logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
-        for logger in {'boto3', 'botocore'}:
-            logging.getLogger(logger).setLevel(logging.INFO)
+        for logger in {
+                'boto3', 'botocore', 'requests',
+                'botocore.vendored.requests.packages.urllib3.connectionpool'}:
+            logging.getLogger(logger).setLevel(logging.WARNING)
     backup(args)
 
 
@@ -94,20 +133,31 @@ def parse_cli_args():
     parser = argparse.ArgumentParser(
         'dynamodb-backup',
         description='Backup a DynamoDB table to an Avro Container')
+
     parser.add_argument('-c', '--codec', choices=CODEC_CHOICES,
                         help='Compression Codec. Default: deflate',
                         default='deflate')
+
+    parser.add_argument('-b', '--bucket', action='store',
+                        help='S3 Bucket to upload the file to')
+
+    parser.add_argument('-d', '--destination', action='store',
+                        help='Local path to write the file to.')
+
+    parser.add_argument('-p', '--prefix', action='store', default='dynamodb',
+                        help='The S3 Bucket base path to use. '
+                             'Default: dynamodb/')
+
     parser.add_argument('-s', '--skip-validation', action='store_true',
                         help='Do not validate records against the avro schema')
+
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Verbose logging output')
+
     parser.add_argument('schema', action='store',
-                        type=argparse.FileType('r'),
                         help='Avro Schema to use')
     parser.add_argument('table', action='store',
                         help='DynamoDB table name')
-    parser.add_argument('destination', action='store',
-                        help='Destination file path for the backup file')
     return parser.parse_args()
 
 
@@ -164,6 +214,21 @@ def _unmarshall_dict(value):
     elif key == 'SS':
         return set([v for v in value[key]])
     raise ValueError('Unsupported value type: %s' % key)
+
+
+def _upload_to_s3(args, handle):
+    """Create the backup as a file on AmazonS3
+
+    :param argparse.namespace args: The parsed CLI arguments
+    :param io.BytesIO handle: The BytesIO handle for the backup
+
+    """
+    handle.seek(0)
+    backup_path = datetime.date.today().strftime(
+        '{}/%Y/%m/%d/{}.avro'.format(args.prefix, args.table))
+    s3 = boto3.resource('s3')
+    s3.Object(args.bucket, backup_path).put(Body=handle)
+
 
 if __name__ == '__main__':
     main()
